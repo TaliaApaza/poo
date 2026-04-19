ce4955d749007abc5fb4259b18743fb5dcae8551
TaliaApaza <taliaapaza90@gmail.com>
2026-04-19 18:05:18 +0000

    Alteração de hje

--- Assunto_02/teste.py
+++ Assunto_02/teste.py
@@ -1,73 +1,53 @@
-class Conta_Bancaria:
+class Ingresso:
     def __init__(self):
-        self.__nome = ""
-        self.__numero_conta= 0
-        self.__saldo= 0.0
-        self.__deposito=0.0
-        self.__saque=0.0
-
-    def set_nome(self, v):
-        if len(v) > 3:
-            self.__nome = v
-        else:
-            raise ValueError()
-
-    def set_numero_conta(self, v):
-        if v >= 0: 
-            self.__saldo = v
-        else:
-            raise ValueError()
-        
-    def set_saldo(self, v):
-        if v == 0.0: 
-            self.__saldo = v
-        else:
-            raise ValueError()
-        
-    def set_deposito(self, v):
-        if v >= 0:
-            self.__deposito = v
-            return self.__saldo== self.__saldo + self.__deposito
+        self.__dia = ""
+        self.__horario = 0
+    def set_dia(self, v):
+        if len(v)>=4:
+            self.__dia = v
         else: raise ValueError()
 
-    def set_saque(self, v):
-        if v >= 0: 
-            self.__saldo = v
-            return self.__saldo== self.__saldo - self.__saque
+    def set_horario(self,v):
+        if self.__horario >= 0:
+            self.__horario = v
+        else: raise ValueError()
+    def get_dia(self):
+        return self.__dia
     
-    def get_nome(self):
-        return self.__nome
+    def get_horario(self):
+        return self.__horario
     
-    def get_numero_conta(self):
-        return self.__numero_conta
-    def get_saldo(self):
-        return self.__numero_conta
+    def calc_ingresso(self):
+        if self.__dia == "Segunda" or self.__dia == "Terça" or self.__dia == "Quinta":
+            ingresso = 16.00
+            if self.__horario>=17:
+                ingresso = ingresso* 1.5
+
+        elif self.__dia == "Sexta" or self.__dia == "Sábado" or self.__dia == "Domingo":
+            ingresso = 20.00
+            if self.__horario >= 17:
+                ingresso = ingresso * 1.5
+            
+        elif self.__dia == "Quarta":
+            ingresso = 8.00
+        
+        else: raise ValueError()
+        
+        return ingresso
     
-    def get_deposito(self):
-        return self.__deposito
-    
-    def get_saque(self):
-     return self.__saque
-    
-    def depositar(self):
-        self.__deposito== self.__saldo + self.__deposito
-        return self.__deposito
-    
-    def sacar(self):
-        self.__saque==  self.__saque - self.__saldo 
-        return self.__saque
-    
-x = Conta_Bancaria()
-x.set_nome = input("Nome: ")
-x.set_numero_conta = int(input("NUmero da conta: "))
-x.set_saldo = float(input("digite 0.0 "))
-print("Escolha uma opção sendo 1 = deposito e 2 = saque")
-opcao= int(input())
-if opcao==1:
-    x.set_deposito(float(input("deposito")))
-    print(f"saldo {x.depositar()}")
-elif opcao==2:
-    x.set_saque(float(input("saque")))
-    print(f"nome {x.get_nome()} numero da conta {x.get_numero_conta()} saldo{x.sacar()}")
-else: 
-    print("opção invalida")
+x = Ingresso()
+x.set_dia (input( "Escreva um dia da semana(ex: Sábado): "))
+x.set_horario (int(input("Escreva um Horario (ex. 17)")))
+
+print(f"Dia: {x.get_dia()}. Horario: {x.get_horario()}h. valor do ingresso: {x.calc_ingresso()}")
+
+        
+        
+
+
+
+
+
+
+
+

99f297fa6f2ded6e09188c97fc4492e02d9434b1
TaliaApaza <taliaapaza90@gmail.com>
2026-05-11 19:52:10 +0000

    babado

--- atividades_extras/atividade_bingo.py
+++ atividades_extras/atividade_bingo.py
@@ -6,18 +6,22 @@ class Bingo():
         self.sorteados = []
     def iniciar(self):
         if self.nu_bolas > 0:
-            for a in range(1, self.nu_bolas + 1):
+            for a in range(1, self.nu_bolas + 1):#inicio sendo 1 e o fim sendo o valor informado mais 1
                 self.bolas.append(a)
         else: raise ValueError("numero invalido")
         return self.bolas
     
     def sortear(self):
-          bola = random.choice(self.bolas)
+          bola = random.choice(self.bolas)#retorna o valor sorteados
+          self.sorteados.append(bola)
+          self.bolas.remove(bola)
+          return bola
     def sorteado(self):
         return self.sorteados
     
 
 class UI:
+    x = Bingo()#o bingo se torna geral e não é necessario chala novamente
     @staticmethod
     def main():
         op = 0 #cria a variavel opção
@@ -27,20 +31,21 @@ class UI:
             if op ==2: UI.sortear_num()
             if op == 3: UI.num_sorteados()
         print("fim")
+    @staticmethod
     def menu():
         print("Escolha uma opção: 1-iniciar novo jogo, 2-sortear um número, 3-listar numeros sorteados")
         op = int(input("informe a opção: "))
         return op
-    
+    @staticmethod
     def iniciar_jogo():
-        x = Bingo()
-        x.nu_bolas = int(input("informe o numero de bolas"))
-        print(f"o total de bolas a serem sorteadas é: {x.iniciar()}")
-
+        UI.x.nu_bolas = int(input("informe o numero de bolas"))
+        print(f"o total de bolas a serem sorteadas é: {UI.x.iniciar()}")
+    @staticmethod
     def sortear_num():
-        x = Bingo()
-        x.nu_bolas = int(input("informe o numero de bolas"))
-        print(f"bola sorteada será: {x.sortear()}")
+        print(f"numero sorteado: {UI.x.sortear()}")
+    @staticmethod
+    def num_sorteados():
+        print(f"numeros sorteados: {UI.x.sorteado()}")
 
 UI.main()
         

0b781e9ac40cdae0dde208dbaa3d7600347f22ae
TaliaApaza <taliaapaza90@gmail.com>
2026-05-11 14:02:18 +0000

    auajhdj

--- atividades_extras/atividade_bingo.py
+++ atividades_extras/atividade_bingo.py
@@ -5,18 +5,26 @@ class Bingo:
         self.bolas = []
         self.sorteados = []
     def iniciar(self):
-        if self.nu_bola >= 0:
+        if self.nu_bolas > 0:
             a = 0
-            while a <= self.nu_bolas:
+            while a <= self.nu_bolas - 1:
                 a += 1
                 self.bolas.append(a)
         else: raise ValueError("numero invalido")
     def sortear(self):
         random.shuffle(self.bolas)#embaralha a lista que contem as bolas
-        b = self#pop para retirar um elemento
+        b = self.bolas.pop()#pop para retirar um elemento e b irá amarzenar esses elementos
         self.sorteados.append(b)
         return b
     
     def sorteado(self):
         return self.sorteados
+    
+
+class UI:
+    @staticmethod
+    def main():
+        op = 0 #cria a variavel opção
+        while op !=
+        
 
